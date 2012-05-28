a2e59a66ccce7e475624049c5c0c29e07646de90
Johnnie Pittman <jpittman@group6.net>
2012-05-28 09:37:38 -0700

    Adding a view controller to manage the UIImagePickerController.
    Also adding in a subclass of a uibutton for our raised button.
    And some art.

---
+++ RaisedCenterButton/ImagePickerViewController.m
@@ -0,0 +1,44 @@
+//
+//  ImagePickerViewController.m
+//  RaisedCenterButton
+//
+//  Created by Johnnie Pittman on 5/13/12.
+//  Copyright (c) 2012 Group 6. All rights reserved.
+//
+
+#import "ImagePickerViewController.h"
+
+@interface ImagePickerViewController ()
+
+@end
+
+@implementation ImagePickerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
---
+++ RaisedCenterButton/RaisedCenterButton.m
@@ -0,0 +1,13 @@
+//
+//  RaisedCenterButton.m
+//  RaisedCenterButton
+//
+//  Created by Johnnie Pittman on 5/13/12.
+//  Copyright (c) 2012 Group 6. All rights reserved.
+//
+
+#import "RaisedCenterButton.h"
+
+@implementation RaisedCenterButton
+
+@end
